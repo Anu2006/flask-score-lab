2fcac29d6122c6c251571259c2851584b0323cf4
Anu2006 <55542548+Anu2006@users.noreply.github.com>
2020-01-03 21:12:49 +0530

    Update first_app.py

--- flask web-app/first_app.py
+++ flask web-app/first_app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask  # importing Flask class from flask
 
-app = Flask(__name__)
+app = Flask(__name__)  # name is a variable that stores the module name. we pass it to the Flask class
+# as an argument to create our flask server. other things it will configure it self.
 
 
-@app.route('/')
+@app.route('/')  # app.route is a decorater that tells the function page it lands. here this function will run on main page
 def hello_world():
     return '<h1> Hello World <h1 />'
 
@@ -13,5 +14,5 @@ def home():
     return '<h2> Welcome home fellow developers <h2 />'
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":  # if this script is not imported from another module
+    app.run(debug=True)  # run the app with debug mode on
